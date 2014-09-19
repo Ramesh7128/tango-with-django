3f1696c9fe8cce57923d08b91a6d6c18c558a6e0
Ramesh7128 <ramesh7128@gmail.com>
2014-09-20 02:01:31 +0530

    second commit

--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,12 @@
 # Create your views here.
 from django.http import HttpResponse
+from django.template import RequestContext
 from django.shortcuts import render_to_response
 
 def index(request):
-    context =RequestContext(request)
+    context = RequestContext(request)
     context_dict = {'boldmessage': "i am bold from context"}
     return render_to_response('rango/index.html', context_dict, context)
 
 def about(request):
-    return HttpResponse("Rango says: here is about page<a href=/rango/>index</a>")
+    return HttpResponse("Rango sys: here is about page<a href=/rango/>index</a>")
