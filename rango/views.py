b3e68b5874cb329fce43a971601c5dfe24101256
Ramesh7128 <ramesh7128@gmail.com>
2014-09-21 15:03:31 +0530

    index page with list of categories

--- rango/views.py
+++ rango/views.py
@@ -2,11 +2,14 @@
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from rango.models import Category
+
 
 
 def index(request):
     context = RequestContext(request)
-    context_dict = {'boldmessage': "i am bold from context"}
+    category = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category}
     return render_to_response('rango/index.html', context_dict, context)
 
 
